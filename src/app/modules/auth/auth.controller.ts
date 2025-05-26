628f0fd40af8e6c980429a00ff5c2a0c3ad10cb8
shaheen2013 <coredeveloper.2013@gmail.com>
2025-05-26 20:16:30 +0600

    logger used inside the auth controller for testing purpose

--- src/app/modules/auth/auth.controller.ts
+++ src/app/modules/auth/auth.controller.ts
@@ -1,10 +1,12 @@
 import { Body, Controller, Get, Post } from '@nestjs/common';
+import { CustomLogger } from '../logger/logger.service';
 import { CreateUserDto } from '../user/dto/create-user.dto';
 import { AuthService } from './auth.service';
 
 @Controller('api/v1')
 export class AuthController {
   constructor(private readonly authService: AuthService) {}
+  private readonly logger = new CustomLogger(AuthController.name);
 
   @Post('/register')
   async credentioalLogin(@Body() dto: CreateUserDto) {
@@ -14,5 +16,15 @@ export class AuthController {
   @Get('/test-grpc')
   testGrpc() {
     return this.authService.getAuthrization({ email: 'test@hudai.com' });
+  }
+
+  @Get('/test-logger')
+  testLogger() {
+    this.logger.log('test logger >>>>');
+    this.logger.error('test logger error >>>>');
+    this.logger.warn('test logger warn >>>>');
+    this.logger.debug('test logger debug >>>>');
+    this.logger.verbose('test logger verbose >>>>');
+    return 'test logger response!';
   }
 }
