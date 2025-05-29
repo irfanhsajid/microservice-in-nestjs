14563ab63a42ccdf419488fef8eda93d02195ccd
irfanhsajid <irfanhsajid@gmail.com>
2025-05-29 19:58:35 +0600

    login throttler setup in the docs module

--- src/app/modules/docs/docs.modules.ts
+++ src/app/modules/docs/docs.modules.ts
@@ -1,7 +1,9 @@
 import { Module } from '@nestjs/common';
+import { LoginThrottlerGuard } from '../throttler/login-throttler.guard';
 import { DocsAuthController } from './docs.controller';
 
 @Module({
   controllers: [DocsAuthController],
+  imports: [LoginThrottlerGuard],
 })
 export class DocsModule {}
