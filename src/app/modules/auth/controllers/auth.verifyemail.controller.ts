db200b5728b43c6a29f6b52e210fd8f2ae8420e0
kain <chakma.kalin10211@gmail.com>
2025-06-18 13:49:07 +0600

    addred auth guard to verify-email and resend verification email

--- src/app/modules/auth/controllers/auth.verifyemail.controller.ts
+++ src/app/modules/auth/controllers/auth.verifyemail.controller.ts
@@ -1,20 +1,26 @@
-import { ApiOperation, ApiTags } from '@nestjs/swagger';
-import { Body, Controller, Post } from '@nestjs/common';
+import { ApiBearerAuth, ApiOperation, ApiTags } from '@nestjs/swagger';
+import { Body, Controller, Post, UseGuards } from '@nestjs/common';
 import { AuthService } from '../services/auth.service';
 import { VerifyEmailDto } from '../dto/verify-email.dto';
 import { ResendVerifyEmailDto } from '../dto/resend-verify-email.dto';
+import { ApiGuard } from '../api.guard';
 
 @ApiTags('Auth')
 @Controller('api/v1')
 export class VerifyEmailController {
   constructor(private readonly authService: AuthService) {}
 
+  @UseGuards(ApiGuard)
   @ApiOperation({ summary: 'Verify email address' })
+  @ApiBearerAuth('jwt')
   @Post('/verify-email')
   async verifyEmail(@Body() dto: VerifyEmailDto) {
     return await this.authService.verifyEmail(dto);
   }
 
+  @UseGuards(ApiGuard)
+  @ApiOperation({ summary: 'Resend verification email' })
+  @ApiBearerAuth('jwt')
   @Post('/resend-verification-email')
   async resendVerificationEmail(@Body() dto: ResendVerifyEmailDto) {
     return await this.authService.resendVerificationEmail(dto);
