ec854ee12f18f768e77fc1fefe28a66cb48020ea
shaheen2013 <coredeveloper.2013@gmail.com>
2025-06-18 14:59:01 +0600

    2. Dealership Onboarding payment info flow 	i. add dealership payment info controller 	ii. add dealership payment info service, interface

--- src/app/modules/dealership/controllers/dealership.paymentinfo.controller.ts
+++ src/app/modules/dealership/controllers/dealership.paymentinfo.controller.ts
@@ -1,10 +1,13 @@
-import { Controller, Post, Get } from '@nestjs/common';
-import { ApiOperation, ApiTags } from '@nestjs/swagger';
+import { Controller, Post, Get, UseGuards } from '@nestjs/common';
+import { ApiBearerAuth, ApiOperation, ApiTags } from '@nestjs/swagger';
 import { CustomLogger } from '../../logger/logger.service';
 import { DealershipPaymentInfo } from '../entities/dealership-payment-info.entity';
 import { DealershipPaymentInfoService } from '../services/dealership.paymentinfo.service';
+import { ApiGuard } from '../../../guards/api.guard';
 
 @ApiTags('Onboarding')
+@ApiBearerAuth('jwt')
+@UseGuards(ApiGuard)
 @Controller('api/v1')
 export class DealershipPaymentInfoController {
   constructor(protected paymentInfoService: DealershipPaymentInfoService) {}
@@ -15,12 +18,12 @@ export class DealershipPaymentInfoController {
   @ApiOperation({ summary: 'Dealership bank payment info' })
   @Get('/payment-info')
   async show(): Promise<DealershipPaymentInfo> {
-    return this.paymentInfoService.show();
+    return await this.paymentInfoService.show();
   }
 
   @ApiOperation({ summary: 'Dealership bank payment info update' })
   @Post('/payment-info')
   async update(): Promise<DealershipPaymentInfo> {
-    return this.paymentInfoService.updateOrCreate();
+    return await this.paymentInfoService.updateOrCreate();
   }
 }
