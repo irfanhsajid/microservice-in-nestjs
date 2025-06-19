2ae1eeda9e59afba568410029176ec0ab03e0bd5
kain <chakma.kalin10211@gmail.com>
2025-06-19 15:28:36 +0600

    revert remove code

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -23,6 +23,7 @@ import { EnsureEmailVerifiedGuard } from './guards/ensure-email-verified.guard';
 import { DealershipModule } from './modules/dealership/dealership.module';
 import { AddressModule } from './modules/address/address.module';
 import { AttachmentModule } from './modules/attachment/attachment.module';
+import { AppController } from './app.controller';
 
 @Module({
   imports: [
@@ -94,7 +95,7 @@ import { AttachmentModule } from './modules/attachment/attachment.module';
     AddressModule,
     AttachmentModule,
   ],
-  controllers: [],
+  controllers: [AppController],
   providers: [ApiGuard, EnsureEmailVerifiedGuard],
 })
 export class AppModule {}
