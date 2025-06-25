eaed664eb86d61af4ac7519ea5828b07a40aad1f
shaheen2013 <coredeveloper.2013@gmail.com>
2025-06-25 20:16:48 +0600

    feat: implement user dealer seeder functionality.

--- src/database/seeders/userSeeder.ts
+++ src/database/seeders/userSeeder.ts
@@ -4,10 +4,15 @@ import {
 } from '../../app/modules/user/entities/user.entity';
 import { DataSource } from 'typeorm';
 import * as bcrypt from 'bcrypt';
+import { UserDealership } from '../../app/modules/dealership/entities/user-dealership.entity';
+import { Role } from '../../app/modules/roles/entities/role.entity';
 
 const userSeeder = async (conn: DataSource) => {
   const userRepo = conn.getRepository(User);
+  const userDealershipRepo = conn.getRepository(UserDealership);
+  const roleRepo = conn.getRepository(Role);
 
+  // Step 1: Create user
   const data = [
     {
       password: await hashPassword('12345'),
@@ -18,12 +23,26 @@ const userSeeder = async (conn: DataSource) => {
       status: true,
       email_verified_at: new Date(),
       account_type: UserAccountType.MODERATOR,
-      role_id: 1,
     },
   ];
 
   await userRepo.save(data);
-  console.log('User seeded');
+
+  const user = await userRepo.findOne({
+    where: { email: 'carvu@gmail.com' },
+  });
+
+  // Step 2: Create user dealership
+
+  // Get admin role id
+  const role = await roleRepo.findOne({
+    where: { name: 'admin' },
+  });
+  await userDealershipRepo.save({
+    role_id: role?.id,
+    user_id: user?.id,
+  });
+  console.log('User and User dealership seeded');
 };
 
 const hashPassword = async (pass: string) => {
