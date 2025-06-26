939ef0aef7cc1ab786fe550dba7480163dc66954
shaheen2013 <coredeveloper.2013@gmail.com>
2025-06-26 16:36:37 +0600

    feat: implement admin dealership service, controller, module and update dealership status API.

---
+++ src/app/modules/admin/dealership/controllers/dearship.controller.ts
@@ -0,0 +1,46 @@
+import { Body, Controller, Param, Patch, Req, UseGuards } from '@nestjs/common';
+import { ApiBearerAuth, ApiTags } from '@nestjs/swagger';
+import { throwCatchError } from 'src/app/common/utils/throw-error';
+import { ApiGuard } from 'src/app/guards/api.guard';
+import { CustomLogger } from 'src/app/modules/logger/logger.service';
+import { UpdateDealershipStatusDto } from '../dto/update-dealership-status.dto';
+import { AdminDealershipService } from '../services/dealership.service';
+import { responseReturn } from 'src/app/common/utils/response-return';
+import { AbilityGuard } from 'src/app/modules/auth/casl/ability.guard';
+import { CheckAbility } from 'src/app/modules/auth/casl/check-ability.decorator';
+
+@ApiTags('Admin Dealership Management')
+@ApiBearerAuth('jwt')
+@UseGuards(ApiGuard)
+@Controller('api/v1/admin/dealerships')
+export class AdminDealershipController {
+  constructor(
+    private readonly adminDealershipService: AdminDealershipService,
+  ) {}
+  private readonly logger = new CustomLogger(AdminDealershipController.name);
+
+  @UseGuards(AbilityGuard)
+  @CheckAbility('update', 'dealership')
+  @Patch('/:dealer_id/status')
+  async updateStatus(
+    @Req() req: Request,
+    @Param('dealer_id') dealer_id: string,
+    @Body() dto: UpdateDealershipStatusDto,
+  ) {
+    try {
+      const dealership =
+        await this.adminDealershipService.updateDealershipStatus(
+          req,
+          +dealer_id,
+          dto,
+        );
+      return responseReturn(
+        'Dealership status updated successfully',
+        dealership,
+      );
+    } catch (error) {
+      this.logger.error(error);
+      return throwCatchError(error);
+    }
+  }
+}
---
+++ src/app/modules/admin/dealership/services/dealership.service.ts
@@ -0,0 +1,55 @@
+import { Injectable, NotFoundException } from '@nestjs/common';
+import { ServiceInterface } from 'src/app/common/interfaces/service.interface';
+import { UpdateDealershipStatusDto } from '../dto/update-dealership-status.dto';
+import { UserDealership } from 'src/app/modules/dealership/entities/user-dealership.entity';
+import { Repository } from 'typeorm';
+import { InjectRepository } from '@nestjs/typeorm';
+
+@Injectable()
+export class AdminDealershipService implements ServiceInterface {
+  constructor(
+    @InjectRepository(UserDealership)
+    private readonly userDealershipRepository: Repository<UserDealership>,
+  ) {}
+
+  destroy(req: Request, id: number): Record<string, any> {
+    throw new Error('Method not implemented.');
+  }
+
+  index(req: Request, params: any): Record<string, any> {
+    throw new Error('Method not implemented.');
+  }
+
+  show(req: Request, id: number): Record<string, any> {
+    throw new Error('Method not implemented.');
+  }
+
+  store(req: Request, dto: any): Record<string, any> {
+    throw new Error('Method not implemented.');
+  }
+
+  update(req: Request, dto: any, id: number): Record<string, any> {
+    throw new Error('Method not implemented.');
+  }
+
+  async updateDealershipStatus(
+    req: Request,
+    dealerId: number,
+    dto: UpdateDealershipStatusDto,
+  ) {
+    const userDealership = await this.userDealershipRepository.findOne({
+      where: {
+        dealership_id: dealerId,
+      },
+    });
+
+    if (!userDealership) {
+      throw new NotFoundException(`Dealership with ID ${dealerId} not found`);
+    }
+
+    userDealership.status = dto.status;
+    await this.userDealershipRepository.save(userDealership);
+
+    return userDealership;
+  }
+}
