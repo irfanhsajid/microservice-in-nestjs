a7527d54cd0bed73ca41d293567a37392cc0bef6
shaheen2013 <coredeveloper.2013@gmail.com>
2025-06-26 13:12:52 +0600

    fix: correct spelling of DealershipAttachmentDto and DealershipAttachmentFileType

--- src/app/modules/dealership/dto/dealership-attachment.dto.ts
+++ src/app/modules/dealership/dto/dealership-attachment.dto.ts
@@ -1,7 +1,7 @@
 import { ApiProperty } from '@nestjs/swagger';
 import { IsEnum, IsNotEmpty } from 'class-validator';
 
-export enum DealershipAttachementFileType {
+export enum DealershipAttachmentFileType {
   OMVIC_DEALER_LICENSE = 'OMVIC_DEALER_LICENSE',
   BUSINESS_VOID_CHEQUE = 'BUSINESS_VOID_CHEQUE',
   DRIVERS_LICENSE = 'DRIVERS_LICENSE',
@@ -9,9 +9,9 @@ export enum DealershipAttachementFileType {
   PAD_OR_PAW_AUTHORIZATION_FORM = 'PAD_OR_PAW_AUTHORIZATION_FORM',
 }
 
-export class DealershipAttachementDto {
+export class DealershipAttachmentDto {
   @ApiProperty()
   @IsNotEmpty()
-  @IsEnum(DealershipAttachementFileType)
-  name: DealershipAttachementFileType;
+  @IsEnum(DealershipAttachmentFileType)
+  name: DealershipAttachmentFileType;
 }
