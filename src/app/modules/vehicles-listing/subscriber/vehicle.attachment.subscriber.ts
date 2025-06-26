cb06a550e2791e1d43f2e2fc990abb8021aebca5
shaheen2013 <coredeveloper.2013@gmail.com>
2025-06-26 18:58:22 +0600

    add vehicle subscription

---
+++ src/app/modules/vehicles-listing/subscriber/vehicle.attachment.subscriber.ts
@@ -0,0 +1,24 @@
+import { EntitySubscriberInterface, EventSubscriber } from 'typeorm';
+import { Injectable } from '@nestjs/common';
+import { FileUploaderService } from '../../uploads/file-uploader.service';
+import { VehicleAttachment } from '../entities/vehicle-attachments.entity';
+
+@Injectable()
+@EventSubscriber()
+export class VehicleAttachmentSubscriber
+  implements EntitySubscriberInterface<VehicleAttachment>
+{
+  constructor(private readonly fileUploadService: FileUploaderService) {}
+
+  listenTo() {
+    return VehicleAttachment;
+  }
+
+  afterLoad(entity: VehicleAttachment) {
+    if (entity.path && entity.vehicle_id) {
+      entity.path = this.fileUploadService.path(
+        `vehicle/${entity.vehicle_id}/${entity.path}`,
+      );
+    }
+  }
+}
