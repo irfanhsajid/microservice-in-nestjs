ad491399d7ae5bb00e7eb92ddd263f4eca9aad9f
shaheen2013 <coredeveloper.2013@gmail.com>
2025-07-07 15:36:29 +0600

    feat: API implement for getting user with proper response and pagination and also implement searching.

--- src/app/modules/admin/user/services/user.service.ts
+++ src/app/modules/admin/user/services/user.service.ts
@@ -1,10 +1,16 @@
 import { Injectable, NotFoundException } from '@nestjs/common';
 import { ServiceInterface } from 'src/app/common/interfaces/service.interface';
 import { PaginationEnum } from '../../common/enums/pagination.enum';
+import { User } from 'src/app/modules/user/entities/user.entity';
+import { InjectRepository } from '@nestjs/typeorm';
+import paginate from 'src/app/common/pagination/paginate';
+import { ILike, Repository } from 'typeorm';
 
 @Injectable()
 export class AdminUserService implements ServiceInterface {
-  constructor() {}
+  constructor(
+    @InjectRepository(User) private readonly userRepository: Repository<User>,
+  ) {}
 
   destroy(req: Request, id: number): Record<string, any> {
     throw new Error('Method not implemented.');
@@ -14,11 +20,29 @@ export class AdminUserService implements ServiceInterface {
     const page = params.page || PaginationEnum.DEFAULT_PAGE;
     const limit = params.limit || PaginationEnum.DEFAULT_LIMIT;
     const search = params.search || '';
-    const orderBy = params.sort_column || PaginationEnum.DEFAULT_SORT_COLUMN;
-    const orderDirection =
-      params.sort_direction || PaginationEnum.DEFAULT_SORT_ORDER;
 
-    return new Promise((resolve, reject) => {});
+    const usersQUery = this.userRepository
+      .createQueryBuilder('user')
+      .leftJoinAndSelect('user.user_dealerships', 'user_dealerships')
+      .leftJoinAndSelect('user_dealerships.role', 'role')
+      .where('user.name ILIKE :search', { search: `%${search}%` })
+      .select([
+        'user.id',
+        'user.name',
+        'user.email',
+        'user.status',
+        'user_dealerships.status',
+        'role.id',
+        'role.name',
+        'user.created_at',
+      ]);
+
+    const users = await paginate(usersQUery, {
+      page,
+      limit,
+    });
+
+    return users;
   }
 
   async show(req: Request, id: number): Promise<Record<string, any>> {
